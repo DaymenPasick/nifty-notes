ab50edb15eb09eff357927c7b7ff3f9c79ce38af
DaymensCode <Daymenscode@gmail.com>
2024-01-14 17:59:27 -0500

    used app.get to set default sending of client to the views/index.html

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-//Variables for Express, path, and fs modules
+//variables for xxpress, path, and fs modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-//Access uuid in helpers to generate unique IDs 
+//access uuid in helpers to generate unique IDs 
 const uuid = require('./helpers/uuid');
 
-//Set the port to our local server space
+//set the port to our local server space
 const PORT = 3001;
 
 
@@ -18,3 +18,9 @@ app.use(express.urlencoded({extended: true}));
 
 //indicating use of static Middleware and pointing it to public directory
 app.use(express.static('public'));
+
+
+//set the default / client request to the start page in index.html
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+)
