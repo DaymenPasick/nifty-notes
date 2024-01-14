5f82d77b7bc3d33f24b51b9e6516fad0f8075e54
DaymensCode <Daymenscode@gmail.com>
2024-01-14 17:45:27 -0500

    more server setup. enabled json, urlencoded, and static Middle

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 //Variables for Express, path, and fs modules
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 //Access uuid in helpers to generate unique IDs 
-const uuid = require('./helpers/uuid')
+const uuid = require('./helpers/uuid');
 
 //Set the port to our local server space
 const PORT = 3001;
 
 
 const app = express();
+
+//enables handling of json and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//indicating use of static Middleware and pointing it to public directory
+app.use(express.static('public'));
