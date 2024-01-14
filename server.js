6081f7214c100b0382569d9c9ed817b7a3ccdfe0
DaymensCode <Daymenscode@gmail.com>
2024-01-14 17:40:57 -0500

    creating some const requires, and comments to set up local server

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+//Variables for Express, path, and fs modules
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
 
+//Access uuid in helpers to generate unique IDs 
+const uuid = require('./helpers/uuid')
+
+//Set the port to our local server space
+const PORT = 3001;
+
+
+const app = express();
