911acba87d70b09e39ad50c328006370b27f0be6
DaymensCode <Daymenscode@gmail.com>
2024-01-14 14:05:40 -0500

    added issue comments for troubleshooting functionality issues

---
+++ server.js
@@ -0,0 +1,20 @@
+//Get started button should take us to notetaking page 
+
+
+
+//Existing notes should flow to the left of the page when notetaker starts
+
+
+
+//Starting note page should just have a sample Note Title and Note Text space
+//along with a button in top right for new notes
+
+
+//clicking new note button should allow user to click and edit 
+//inside of the note title and note text
+//at this point the new note button should dissapear
+
+//when content is entered in either title or text on the note
+//the clear form button should populate
+
+//when data is typed into text title AND text
