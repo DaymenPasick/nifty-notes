9737ebdf03ca72decdb6cfec3fcfa8de2fe43e3c
DaymensCode <Daymenscode@gmail.com>
2024-01-15 00:29:38 -0500

    cleaned up some comments, removed comments.js file and the guide img in helpers

--- comments.js
+++
@@ -1,8 +0,0 @@
-//save button not populateing new notes to the left column
-//need to seperate creation of persisting database from writting into the database
-
-
-
-
-//need to clear out saved note data from main page body
-//once the save note button has been clicked
