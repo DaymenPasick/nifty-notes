99330945c64df20ceb3f840f7d8433b576329515
DaymensCode <Daymenscode@gmail.com>
2024-01-14 21:50:07 -0500

    woop woop! Back on track! in routes/notes.js I set the db note data to dbData const and returned that with the api/notes client reqest

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,16 @@
-// //this file will handle client reqs for api/notes
+//this file will handle client reqs for api/notes
 
 //specifically calling for the router() in express rather than the entire express module
 const fs = require('fs')
 const notes = require('express').Router()
 const path = require('path');
+const dbData = require('../db/db.json')
 
 //will keep this require here for now incase we want to utilize the helpers/uuid() on this file
 const uuid = require('../helpers/uuid')
 
 //will send user to the main notes html page
-notes.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '../views/notes.html'))
-)
+notes.get('/', (req, res) => res.json(dbData))
 
 
 
