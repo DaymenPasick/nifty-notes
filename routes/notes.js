d62b4bcb17e33b0b9dace707a8e8eb5b07b11e38
DaymensCode <Daymenscode@gmail.com>
2024-01-14 21:52:05 -0500

    changed some comments in routes/notes.js for clarity. working on code to save new notes to databsae

--- routes/notes.js
+++ routes/notes.js
@@ -9,8 +9,12 @@ const dbData = require('../db/db.json')
 //will keep this require here for now incase we want to utilize the helpers/uuid() on this file
 const uuid = require('../helpers/uuid')
 
-//will send user to the main notes html page
+//will send db notes data as a response to client api/notes request
 notes.get('/', (req, res) => res.json(dbData))
+
+
+
+notes.post('/', (req, res) => res.json(dbData))
 
 
 
