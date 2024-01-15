e8b7b29996d47abc506baceca04310292260eaeb
DaymensCode <Daymenscode@gmail.com>
2024-01-14 23:25:43 -0500

    uuid wasn't generating properly because I wrapped my module.export from helpers/uuid in curly braces x.x...fixed it

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,7 +1,7 @@
 //we will use this to generate a unique ID's'
 
-module.exports = () => {
+module.exports = () => 
     Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
       .substring(1);
-};
+
