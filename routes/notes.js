6d40afe0faa99eb69fea3394b1f95ee43f376110
DaymensCode <Daymenscode@gmail.com>
2024-01-14 20:04:22 -0500

    added more code to routes/notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,15 @@
-//
+//this file will handle client reqs for api/notes
+
+//specifically calling for the router() in express rather than the entire express module
+const notes = require('express').Router()
+
+//will keep this require here for now incase we want to utilize the helpers/uuid() on this file
+const uuid = require('../helpers/uuid')
+
+
+
+
+
+
+
+module.exports = notes
