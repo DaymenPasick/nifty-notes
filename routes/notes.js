8762e35c63d6ee8847304185e03d7ac3942c782d
DaymensCode <Daymenscode@gmail.com>
2024-01-14 20:16:50 -0500

    pausing on routers until I figure out issue of client request handling api/note vs notes.html

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,15 @@
 //this file will handle client reqs for api/notes
 
 //specifically calling for the router() in express rather than the entire express module
+const fs = require('fs')
 const notes = require('express').Router()
 
 //will keep this require here for now incase we want to utilize the helpers/uuid() on this file
 const uuid = require('../helpers/uuid')
 
-
+notes.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views/notes.html'))
+)
 
 
 
