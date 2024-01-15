9253f927fcf243320c410327a53a814c9e215b04
DaymensCode <Daymenscode@gmail.com>
2024-01-14 21:02:27 -0500

    deleted more of the troubleshooting comments in comments.js

--- comments.js
+++ comments.js
@@ -1,29 +1,16 @@
 
 //Existing notes should flow to the left of the page when notetaker starts
 
-
-//Starting note page should just have a sample Note Title and Note Text space
-//along with a button in top right for new notes
-
+//
 
 //clicking new note button should allow user to click and edit 
 //inside of the note title and note text
 //at this point the new note button should dissapear
 
-//when content is entered in either title or text on the note
-//the clear form button should populate
-
-//when data is typed into text title AND text fields, 
-//save button should be generated 
 
 //clicking on any of the existing notes should populate that data
 //in the placeholder title and text fields
 //in the center of the page
 //should also populate the new note button
-
-
-//Aesthetic issue, but currently the note title and text fields are not
-//positioned properly
-//They should also have their content displayed without the form borders
 
 
