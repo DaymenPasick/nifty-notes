a4149da4161293b955995bc70c2d851e7d97c65c
DaymensCode <Daymenscode@gmail.com>
2024-01-14 19:52:46 -0500

    trying to find proper logic for notes route setup

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@
 
 //specifically calling for the router() in express rather than the entire express module
 const router = require('express').Router()
+const notesRoute = require('../views/notes.html')
 
 
-router.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'views/notes.html'))
-)
+// router.use('/notes', (req, res) => 
+//     res.sendFile(path.join(__dirname, 'views/notes.html'))
+// )
+
+
+router.use('/notes', notesRoute);
+
+
+module.exports = router
