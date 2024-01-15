a699273432527082577442c7500f21f152e4ca03
DaymensCode <Daymenscode@gmail.com>
2024-01-14 21:44:31 -0500

    cleared some unndeeded code

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const noteDB = require('./db/db.json')
-// const getAndRenderNotes = require('./public/assets/js/index')
+
 
 //access uuid in helpers to generate unique IDs 
 const uuid = require('./helpers/uuid');
