70167285eac25f07aabf2b099f4b409ad5339132
DaymensCode <Daymenscode@gmail.com>
2024-01-14 19:43:58 -0500

    added router.get that SHOULD facilitate api/notes client reqs and take to views/notes.html

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
 // this will facilitate routes starting with /api
+
+//specifically calling for the router() in express rather than the entire express module
+const router = require('express').Router()
+
+
+router.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views/notes.html'))
+)
